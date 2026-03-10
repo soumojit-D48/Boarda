9b623b12bd7a526dbe57761297ad211cc23427b5
Diksha Dhanaji Dabhole <dikshadabhole78@gmail.com>
2026-03-10 21:35:42 +0530

    Implement lazy loading (#70)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 
 import { useAuthStore } from './store/auth-store';
+import Loading from './pages/Loading';
 
-import Landing from './pages/Landing';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import BoardView from './pages/BoardView';
-import Onboarding from './pages/Onboarding';
-import Dashboard from './pages/Dashboard';
-import WorkspaceList from './pages/WorkspaceList';
-import WorkspaceView from './pages/WorkspaceView';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import Profile from './pages/Profile';
+const Landing = lazy(() => import('./pages/Landing'));
+const Signin = lazy(() => import('./pages/Signin'));
+const Signup = lazy(() => import('./pages/Signup'));
+const BoardView = lazy(() => import('./pages/BoardView'));
+const Onboarding = lazy(() => import('./pages/Onboarding'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const WorkspaceList = lazy(() => import('./pages/WorkspaceList'));
+const WorkspaceView = lazy(() => import('./pages/WorkspaceView'));
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
+const ResetPassword = lazy(() => import('./pages/ResetPassword'));
+const Profile = lazy(() => import('./pages/Profile'));
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 import { Navbar } from './components/Navbar';
@@ -33,61 +34,63 @@ function App() {
       <SocketProvider>
         <Navbar />
         <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route
-              path="/onboarding"
-              element={
-                <ProtectedRoute>
-                  <Onboarding />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/workspaces"
-              element={
-                <ProtectedRoute>
-                  <WorkspaceList />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/workspaces/:workspaceId"
-              element={
-                <ProtectedRoute>
-                  <WorkspaceView />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/boards/:boardId"
-              element={
-                <ProtectedRoute>
-                  <BoardView />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route
+                path="/onboarding"
+                element={
+                  <ProtectedRoute>
+                    <Onboarding />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/workspaces"
+                element={
+                  <ProtectedRoute>
+                    <WorkspaceList />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/workspaces/:workspaceId"
+                element={
+                  <ProtectedRoute>
+                    <WorkspaceView />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/boards/:boardId"
+                element={
+                  <ProtectedRoute>
+                    <BoardView />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </Suspense>
         </div>
         <Toaster />
       </SocketProvider>
